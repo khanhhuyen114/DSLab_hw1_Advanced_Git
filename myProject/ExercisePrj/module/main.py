c146d78f634f2d235660287f3394999b9cb6aefe
khanhhuyen <many37758@gmail.com>
2023-09-26 11:08:34 +0700

    update code 3rd tiem

--- myProject/ExercisePrj/module/main.py
+++ myProject/ExercisePrj/module/main.py
@@ -8,6 +8,7 @@ from visualize_dtw import *
 from save_vid import *
 from dynamic_time_warping_module import *
 from highlight_angle import *
+from get_path import *
 
 path1 = "./Sources/viddtw1.mp4"
 path2 = "./Sources/viddtw2.mp4"
@@ -18,10 +19,10 @@ dist_mat = distance_matrix(mat1, mat2)
 
 path = dtw(dist_mat)
 
-output1 = './Sources/odtw1.mp4'
-output2 =  './Sources/odtw2.mp4'
+path_n = get_path1(path)
+output1 = './Sources/odtw1_2.mp4'
+output2 =  './Sources/odtw2_3.mp4'
 stop = 0
 
-visual = visualize_dtw(path1, path2, output1, output2, path)
-print(visual)
-#visual = highlight_angle_fun(path1, path2,path, dist_mat, mat1, mat2)
+#visual = visualize_dtw(path1, path2, output1, output2, path)
+visual = highlight_angle_fun(path1, path2,path_n, mat1, mat2, output1, output2)
